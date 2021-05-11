c18c3dd3243151993b8c25cf5638080f600739b8
Clemens Schueller <schueller.cj@gmail.com>
2021-05-11 10:45:54 +0200

    Card add bookmark

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,15 +1,20 @@
 import * as React from 'react'
 import './Cards.css'
 
-export default function Card ()({title, copyText }) => {
+export default function Card ({title, copyText, isBookmarked }) {
+    const bookmarkClass = isBookmarked
+      ? 'Card__bookmark Card__bookmark--active'
+              : Card__bookmark }
+
     return (
         <section className="Card">
-          <button className={isBookmarked
-             ? 'Card__bookmark" aria-label="Bookmark"></button>
+          <button className={
+              isBookmarked
+               
+              aria-label="Bookmark card">
+          </button>
           <h2 className="Card__title">{title}</h2>
-            <Bookmark active={bookmark}
-            
-            <p className="Card__text">Lorem ipsum dolor sit amet.</p>
+          <p>{copyText</p>
         </section>
     )
 }
