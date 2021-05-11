08dc379725ad73172a48163a4a1e116b3658dc05
Clemens Schueller <schueller.cj@gmail.com>
2021-05-11 10:36:18 +0200

    Card Component already finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import Button from './Button'
 export default () => {
   return (
     <div>
-      <Button isActive={true}>Active</Button>
-      <Button isActive={false}>Not active</Button>
+      <Card title="Hello world" copyText="How are you?"
     </div>
   )
 }
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,2 +1,15 @@
 import * as React from 'react'
 import './Cards.css'
+
+export default function Card ()({title, copyText }) => {
+    return (
+        <section className="Card">
+          <button className={isBookmarked
+             ? 'Card__bookmark" aria-label="Bookmark"></button>
+          <h2 className="Card__title">{title}</h2>
+            <Bookmark active={bookmark}
+            
+            <p className="Card__text">Lorem ipsum dolor sit amet.</p>
+        </section>
+    )
+}
