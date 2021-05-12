26551cfb4c8c85cb7af9cd4601f1df2c5204d85e
Clemens Schueller <schueller.cj@gmail.com>
2021-05-12 15:23:22 +0200

    TodoList interactive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
-import * as React from 'react'
-import Card from './Cards.jsx'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import './App.css'
 
 export default () => {
+  const [todos, setTodos] = useState([])
   return (
     <div className="App">
-      <Card title="Hello world" copyText="Lorem, ipsum dolor." />
-      <Card
-        isBookmarked
-        title="Moin world"
-        copyText="dolor sit amet consectetur?"
-      />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Add todo:
+          <input name="todo" type="text" />
+        </label>
+        <button>Add</button>
+      </form>
+      <ul>
+        {todos.map(({ text, isDone, id }) => (
+          <li onClick={() => toggleIsDone(id)} key={id}>
+            {text} {isDone && 'Häkchen'}
+          </li>
+        ))}
+      </ul>
     </div>
   )
+
+  function toggleIsDone(id) {
+    const index = todos.findIndex(todo => todo.id === id)
+    const todo = todos[index]
+    setTodos([
+      ...todos.slice(0, index),
+      { ...todo, isDone: !todo.isDone },
+      ...todos.slice(index + 1),
+    ])
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const input = form.elements.todo
+    const newTodo = { text: input.value, isDone: false, id: uuidv4() }
+    // spread operator `...todos`
+    setTodos([newTodo, ...todos])
+    form.reset()
+    input.focus()
+  }
 }
